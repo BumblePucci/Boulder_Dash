185646aaafb778fd7771bd2198980be29b0f8bff
Lisa Andres <andresl@cip.ifi.lmu.de>
2018-01-14 17:14:51 +0100

    Update

--- src/Model/Hauptregeln/Gegnerbewegung.java
+++ src/Model/Hauptregeln/Gegnerbewegung.java
@@ -18,6 +18,10 @@ public class Gegnerbewegung implements Observer {
     private int links;
     private int oben;
     private int unten;
+    private int vorne;
+    private int hinten;
+    private int rechteHand;
+    private int linkeHand;
     private int w;
     private int h;
 
@@ -78,11 +82,150 @@ public class Gegnerbewegung implements Observer {
                 map[x][y].getToken().equals(SIEVE) || map[x][y].getToken().equals(SAND);
     }
 
+    //Xling checks
+    private void setEnvironement (int x, int y) {
+        if (map[x][y].getDirection()==1){
+            linkeHand=y-1;
+            rechteHand=y+1;
+            vorne=x+1;
+            hinten=x-1;
+        }
+        else if (map[x][y].getDirection()==2){
+            linkeHand=x-1;
+            rechteHand=x+1;
+            vorne=y-1;
+            hinten=y+1;
+        }
+        else if (map[x][y].getDirection()==3){
+            linkeHand=y+1;
+            rechteHand=y-1;
+            vorne=x-1;
+            hinten=x+1;
+        }
+        else if (map[x][y].getDirection()==4){
+            linkeHand=x+1;
+            rechteHand=x-1;
+            vorne=y+1;
+            hinten=y-1;
+        }
+    }
+
+
+    private boolean checkFourWalkForwardXlingHori(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) && map[vorne][y].getToken().equals(PATH) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                !(map[vorne][rechteHand].getToken().equals(PATH) || map[vorne][rechteHand].getToken().equals(ME)));
+    }
+
+    private boolean checkFourWalkForwardXlingVerti(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) && map[x][vorne].getToken().equals(PATH) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                !(map[rechteHand][vorne].getToken().equals(PATH) || map[rechteHand][vorne].getToken().equals(ME)));
+    }
+
+    private boolean checkFourWalkForwardAndTurnXlingHori(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) && map[vorne][y].getToken().equals(PATH) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                map[vorne][rechteHand].getToken().equals(PATH));
+    }
+
+    private boolean checkFourWalkForwardAndTurnXlingVerti(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) && map[x][vorne].getToken().equals(PATH) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                map[rechteHand][vorne].getToken().equals(PATH));
+    }
+
+    private boolean checkSixWalkLeftAndTurnXlingHori(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[vorne][y].getToken().equals(PATH) || map[vorne][y].getToken().equals(ME)) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                !(map[vorne][linkeHand].getToken().equals(PATH) || map[vorne][linkeHand].getToken().equals(ME)) &&
+                map[x][linkeHand].getToken().equals(PATH));
+    }
+
+    private boolean checkSixWalkLeftAndTurnXlingVerti(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[x][vorne].getToken().equals(PATH) || map[x][vorne].getToken().equals(ME)) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                !(map[linkeHand][vorne].getToken().equals(PATH) || map[linkeHand][vorne].getToken().equals(ME)) &&
+                map[linkeHand][y].getToken().equals(PATH));
+    }
+
+    private boolean checkSixWalkLeftXlingHori(int x, int y) {
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[vorne][y].getToken().equals(PATH) || map[vorne][y].getToken().equals(ME)) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                map[vorne][linkeHand].getToken().equals(PATH) &&
+                map[x][linkeHand].getToken().equals(PATH));
+    }
+
+    private boolean checkSixWalkLeftXlingVerti (int x, int y) {
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[x][vorne].getToken().equals(PATH) || map[x][vorne].getToken().equals(ME)) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                map[linkeHand][vorne].getToken().equals(PATH) &&
+                map[linkeHand][y].getToken().equals(PATH));
+    }
+
+    private boolean checkNineWalkAndTurnBackXlingHori (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[vorne][y].getToken().equals(PATH) || map[vorne][y].getToken().equals(ME)) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                !(map[x][linkeHand].getToken().equals(PATH) || map[x][linkeHand].getToken().equals(ME)) &&
+                !(map[hinten][linkeHand].getToken().equals(PATH) || map[hinten][linkeHand].getToken().equals(ME)) &&
+                map[hinten][y].getToken().equals(PATH));
+    }
+
+    private boolean checkNineWalkAndTurnBackXlingVerti (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[y][vorne].getToken().equals(PATH) || map[y][vorne].getToken().equals(ME)) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                !(map[linkeHand][y].getToken().equals(PATH) || map[linkeHand][y].getToken().equals(ME)) &&
+                !(map[linkeHand][hinten].getToken().equals(PATH) || map[linkeHand][hinten].getToken().equals(ME)) &&
+                map[x][hinten].getToken().equals(PATH));
+    }
+
+    private boolean checkNineWalkBackAndTurnXlingHori (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[vorne][y].getToken().equals(PATH) || map[vorne][y].getToken().equals(ME)) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                !(map[x][linkeHand].getToken().equals(PATH) || map[x][linkeHand].getToken().equals(ME)) &&
+                map[hinten][linkeHand].getToken().equals(PATH) &&
+                map[hinten][y].getToken().equals(PATH));
+    }
+
+    private boolean checkNineWalkBackAndTurnXlingVerti (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[y][vorne].getToken().equals(PATH) || map[y][vorne].getToken().equals(ME)) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                !(map[linkeHand][y].getToken().equals(PATH) || map[linkeHand][y].getToken().equals(ME)) &&
+                map[linkeHand][hinten].getToken().equals(PATH) &&
+                map[x][hinten].getToken().equals(PATH));
+    }
+
+    private boolean checkNineBlockedHori (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[vorne][y].getToken().equals(PATH) || map[vorne][y].getToken().equals(ME)) &&
+                !(map[x][rechteHand].getToken().equals(PATH) || map[x][rechteHand].getToken().equals(ME)) &&
+                !(map[x][linkeHand].getToken().equals(PATH) || map[x][linkeHand].getToken().equals(ME)) &&
+                !(map[hinten][y].getToken().equals(PATH) || map[hinten][y].getToken().equals(ME)));
+    }
+
+    private boolean checkNineBlockedVerti (int x, int y){
+        return (map[x][y].getToken().equals(XLING) &&
+                !(map[y][vorne].getToken().equals(PATH) || map[y][vorne].getToken().equals(ME)) &&
+                !(map[rechteHand][y].getToken().equals(PATH) || map[rechteHand][y].getToken().equals(ME)) &&
+                !(map[linkeHand][y].getToken().equals(PATH) || map[linkeHand][y].getToken().equals(ME)) &&
+                !(map[x][hinten].getToken().equals(PATH) || map[x][hinten].getToken().equals(ME)));
+    }
+
+
     public void swapling (int x, int y) {
         if (map[x][y].getDirection() == 1) {
             if (checkRowOfTwoTokenHori(x, y, rechts, SWAPLING, PATH)) {
                 map[x][y].setToken(PATH);
                 map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
                 map[rechts][y].setToken(SWAPLING);
                 map[rechts][y].setMoved(1);
                 map[rechts][y].setDirection(1);
@@ -96,37 +239,46 @@ public class Gegnerbewegung implements Observer {
             else if (checkRowOfTwoTokenHori(x,y,rechts,SWAPLING,ME)){
                 map[rechts][y].setToken(GEM);
                 map[rechts][y].setMoved(1);
+                map[rechts][y].setDirection(0);
                 if (checkWallExit(x,oben)) {
                     map[rechts][oben].setToken(GEM);
                     map[rechts][oben].setMoved(1);
+                    map[rechts][oben].setDirection(0);
                 }
                 if (checkWallExit(x,unten)){
                     map[rechts][unten].setToken(GEM);
                     map[rechts][unten].setMoved(1);
+                    map[rechts][unten].setDirection(0);
                 }
                 if (checkWallExit(rechts,y)){
                     map[x+2][y].setToken(GEM);
                     map[x+2][y].setMoved(1);
+                    map[x+2][y].setDirection(0);
                 }
                 if (checkWallExit(links,y)){
                     map[x][y].setToken(GEM);
                     map[x][y].setMoved(1);
+                    map[x][y].setDirection(0);
                 }
                 if (checkWallExit(rechts,oben)){
                     map[x+2][oben].setToken(GEM);
                     map[x+2][oben].setMoved(1);
+                    map[x+2][oben].setDirection(0);
                 }
                 if (checkWallExit(rechts,unten)){
                     map[x+2][unten].setToken(GEM);
                     map[x+2][unten].setMoved(1);
+                    map[x+2][unten].setDirection(0);
                 }
                 if (checkWallExit(links,oben)){
                     map[x][oben].setToken(GEM);
                     map[x][oben].setMoved(1);
+                    map[x][oben].setDirection(0);
                 }
                 if (checkWallExit(links,unten)){
                     map[x][unten].setToken(GEM);
                     map[x][unten].setMoved(1);
+                    map[x][unten].setDirection(0);
                 }
             }
 
@@ -135,6 +287,7 @@ public class Gegnerbewegung implements Observer {
             if (checkRowOfTwoTokenVerti(x, y, rechts, SWAPLING, PATH)) {
                 map[x][y].setToken(PATH);
                 map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
                 map[rechts][y].setToken(SWAPLING);
                 map[rechts][y].setMoved(1);
                 map[rechts][y].setDirection(2);
@@ -152,6 +305,7 @@ public class Gegnerbewegung implements Observer {
             if (checkRowOfTwoTokenHori(x, y, rechts, SWAPLING, PATH)) {
                 map[x][y].setToken(PATH);
                 map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
                 map[rechts][y].setToken(SWAPLING);
                 map[rechts][y].setMoved(1);
                 map[rechts][y].setDirection(3);
@@ -169,6 +323,7 @@ public class Gegnerbewegung implements Observer {
             if (checkRowOfTwoTokenVerti(x, y, rechts, SWAPLING, PATH)) {
                 map[x][y].setToken(PATH);
                 map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
                 map[rechts][y].setToken(SWAPLING);
                 map[rechts][y].setMoved(1);
                 map[rechts][y].setDirection(4);
@@ -181,6 +336,129 @@ public class Gegnerbewegung implements Observer {
             //TODO: swapling strikes
 
 
+        }
+    }
+
+    public void xling (int x, int y) {
+        setEnvironement(x, y);
+        if (map[x][y].getDirection() == 1 || map[x][y].getDirection() == 3) {
+            if (checkNineBlockedHori(x, y)) {
+                map[x][y].setMoved(1);
+                map[x][y].setDirection(map[x][y].getDirection() + 1);
+            } else {
+                if (checkFourWalkForwardXlingHori(x, y)) {
+                    map[vorne][y].setToken(XLING);
+                    map[vorne][y].setMoved(1);
+                    map[vorne][y].setDirection(map[x][y].getDirection());
+
+                } else if (checkFourWalkForwardAndTurnXlingHori(x, y)) {
+                    map[vorne][y].setToken(XLING);
+                    map[vorne][y].setMoved(1);
+                    if (map[x][y].getDirection() == 1) {
+                        map[vorne][y].setDirection(4);
+                    } else {
+                        map[vorne][y].setDirection(2);
+                    }
+
+                } else if (checkSixWalkLeftAndTurnXlingHori(x, y)) {
+                    map[x][linkeHand].setToken(XLING);
+                    map[x][linkeHand].setMoved(1);
+                    if (map[x][y].getDirection() == 1) {
+                        map[x][linkeHand].setDirection(2);
+                    } else {
+                        map[x][linkeHand].setDirection(4);
+                    }
+
+                } else if (checkSixWalkLeftXlingHori(x, y)) {
+                    map[x][linkeHand].setToken(XLING);
+                    map[x][linkeHand].setMoved(1);
+                    map[x][linkeHand].setDirection(map[x][y].getDirection());
+
+                } else if (checkNineWalkAndTurnBackXlingHori(x, y)) {
+                    map[hinten][y].setToken(XLING);
+                    map[hinten][y].setMoved(1);
+                    if (map[x][y].getDirection() == 1) {
+                        map[hinten][y].setDirection(3);
+                    } else {
+                        map[hinten][y].setDirection(1);
+                    }
+
+                } else if (checkNineWalkBackAndTurnXlingHori(x, y)) {
+                    map[hinten][y].setToken(XLING);
+                    map[hinten][y].setMoved(1);
+                    if (map[x][y].getDirection() == 1) {
+                        map[hinten][y].setDirection(2);
+                    } else {
+                        map[hinten][y].setDirection(4);
+                    }
+                }
+                map[x][y].setToken(PATH);
+                map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
+            }
+
+        } else if (map[x][y].getDirection() == 2 || map[x][y].getDirection() == 4) {
+            if (checkNineBlockedVerti(x, y)) {
+                map[x][y].setMoved(1);
+                if (map[x][y].getDirection() == 4) {
+                    map[x][y].setDirection(1);
+                } else {
+                    map[x][y].setDirection(3);
+                }
+            } else {
+                if (checkFourWalkForwardXlingVerti(x, y)) {
+                    map[x][vorne].setToken(XLING);
+                    map[x][vorne].setMoved(1);
+                    map[x][vorne].setDirection(map[x][y].getDirection());
+
+                } else if (checkFourWalkForwardAndTurnXlingVerti(x, y)) {
+                    map[x][vorne].setToken(XLING);
+                    map[x][vorne].setMoved(1);
+                    if (map[x][y].getDirection() == 2) {
+                        map[x][vorne].setDirection(1);
+                    } else {
+                        map[vorne][y].setDirection(3);
+                    }
+
+                } else if (checkSixWalkLeftAndTurnXlingVerti(x, y)) {
+                    map[linkeHand][y].setToken(XLING);
+                    map[linkeHand][y].setMoved(1);
+                    if (map[x][y].getDirection() == 2) {
+                        map[linkeHand][y].setDirection(3);
+                    } else {
+                        map[linkeHand][y].setDirection(1);
+                    }
+
+                } else if (checkSixWalkLeftXlingVerti(x, y)) {
+                    map[linkeHand][y].setToken(XLING);
+                    map[linkeHand][y].setMoved(1);
+                    map[linkeHand][y].setDirection(map[x][y].getDirection());
+                }
+                else if (checkNineWalkAndTurnBackXlingVerti(x,y)){
+                    map[x][hinten].setToken(XLING);
+                    map[x][hinten].setMoved(1);
+                    if (map[x][y].getDirection()==2) {
+                        map[x][hinten].setDirection(4);
+                    }
+                    else {
+                        map[x][hinten].setDirection(2);
+                    }
+                }
+
+                else if (checkNineWalkBackAndTurnXlingVerti(x,y)){
+                    map[x][hinten].setToken(XLING);
+                    map[x][hinten].setMoved(1);
+                    if (map[x][y].getDirection()==2) {
+                        map[x][hinten].setDirection(3);
+                    }
+                    else {
+                        map[x][hinten].setDirection(1);
+                    }
+                }
+                map[x][y].setToken(PATH);
+                map[x][y].setMoved(1);
+                map[x][y].setDirection(0);
+            }
         }
     }
 
